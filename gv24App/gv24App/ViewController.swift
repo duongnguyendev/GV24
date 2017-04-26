e5e4f493885e56d4d16d733e2a00b0cba1cd7bab
Nguyen Duy Duong <duyduong9xx@gmail.com>
2017-04-26 16:39:14 +0700

    Create project

---
+++ gv24App/gv24App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gv24App
+//
+//  Created by Nguyen Duy Duong on 4/26/17.
+//  Copyright © 2017 HBB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
